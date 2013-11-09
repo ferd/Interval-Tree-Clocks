a7d6aad9b4d09ec95723effa1dd6013dd7facb44
Fred Hebert <mononcqc@ferd.ca>
2013-11-09 13:30:22 -0500

    Transform into an Erlang Application
    - Drop C code
    - Drop Java Code
    - Reindent code properly
    - Drop docs relative to C and Java
    - Add .gitignore fitting Erlang
    - Add example run in Erlang

--- README.markdown
+++ README.markdown
@@ -13,96 +13,36 @@ Here we provide reference implementations of ITCs in Java, C and Erlang, and app
 
 
 
-This is just a simple example of how one can use the _Interval_ _Tree_ _Clocks_ library, both in C and JAVA.
-
 ### Demo run
-
-
 
 The image shows a run from ITC witch is divided in sections. Each section represents the state of the system between operations (_fork_, _event_ or _join_) and is labeled with a letter. This letter maps the state of the operations presented in both demo programs.
 
 <a href="http://picasaweb.google.com/lh/photo/07P2CBMlkfauJ651E6eYpQ?feat=embedwebsite"><img src="http://lh3.ggpht.com/_tR0W8QwQsQY/S4ULQBCxDKI/AAAAAAAAAfQ/XW4C9AwOmJc/s800/execFlow.png" /></a>
 
+### Demo code
 
-### Sample code C
+```erlang
 
+A0 = itc:seed(), % a
 
-The initial Stamp must be initialized as a _seed_, after that, the Stamps are modified according to the operation that has been executed.
-All operations use pointers to Stamps and return an _int_ value as a result for the success of the operations, meaning that the resulting Stamp, or Stamps, are returned by reference.
+{A1, B0} = itc:fork(A0), % b, A1 = top, B0 = bottom
 
-```C
-	#include "itc.h"
-	
-	int main(){
-	    stamp* seed = itc_seed(seed);
-		
-	    stamp* a = newStamp(); // a
-	    stamp* b = newStamp();
-	    stamp* c = newStamp();
-		
-	    itc_fork(seed, a, b); // b
-		
-	    itc_event(a, a); // c
-	    itc_event(b, b); // c
-		
-	    itc_fork(a, a, c); // d
-	    itc_event(b, b); // d
-		
-	    itc_event(a, a); // e
-	    itc_join(b, c, b); // e
-		
-	    itc_fork(b, b, c); // f
-		
-	    itc_join(a, b, a); // g
-		
-	    itc_event(a, a); // h
-		
-	    printStamp(a);
-	    printStamp(c);	
-	}
+A2 = itc:event(A1), % c (top)
+B1 = itc:event(B0), % c (bottom)
+
+{A3, C0} = itc:fork(A2), % d (top), A3 = top fork, C0 = bottom fork
+B2 = itc:event(B1), % d (bottom)
+
+A4 = itc:event(A3), % e (top)
+BC0 = itc:join(B2, C0), % e (bottom)
+
+{BC1, D0} = itc:fork(BC0), % f, BC1 = top fork, D0 = bottom fork
+
+ABC0 = itc:join(A4, BC1), % g
+
+ABC1 = itc:event(ABC0). % h
+
 ```
-
-
-### Sample code JAVA
-
-
-
-When using objects, in JAVA, every new Stamp is defined as a seed, meaning that there is no need to apply any method in order to create the seed Stamp. 
-There are alternatives (functional style) to these methods, which are defined in the API.
-
-```java
-	import itc.*;
-	
-	public class teste2 {
-	    public static void main(String[] args){
-	        Stamp a = new Stamp(); // a
-	        Stamp b;
-	        Stamp c;
-	
-	        b = a.fork(); // b
-	
-	        a.event(); // c
-	        b.event(); // c
-	
-	        c = a.fork(); // d
-	        b.event(); // d
-	
-	        a.event(); // e
-	        b.join(c); // e
-	
-	        c = b.fork(); // f
-	
-	        a.join(b); // g
-	
-	        a.event(); // h
-	        
-	        System.out.println(a.toString());
-	        System.out.println(c.toString());
-	    }
-	}
-```
-
-
 
 ## Summary High level presentation of ITCs and its use.
 
@@ -129,57 +69,4 @@ ITCs encode the state needed to track causality in a stamp, composed of an event
 
 (*Peek* is a special case of fork that only copies the event component and creates a new stamp with a null id. It can be used to make messages that transport causal information.)
 
-### Simulating Version Vectors
-
-First replicas need to be created. A seed stamp (with a special id component) is first created and the desired number of replicas can be created by forking this initial seed. Bellow we create 4 replicas (Java objects a,b,c,d - both in imperative and functional style):
-
-```java
-	Stamp a = new Stamp(); // Seed
-	Stamp b = a.fork();
-	Stamp c = a.fork();
-	//or
-	Stamp[] out = Stamp.fork(c);
-	c = out[0];
-	Stamp d = out[1];
-```
-
-(Notice that any stamp can be forked, here we forked stamp a twice and stamp c once.)
-
-Since no events have been registered, these stamps all compare as equal. Since a stamp method leq (_less or equal_) is provided, stamps x and y are equivalent when both x.leq(y) and y.leq(x) are true.
-
-Now, suppose that stamp b is associated to a ReplicaB and this replica was modified. We
-note this by doing:
-
-```java
-	b.event();
-	//or
-	b = Stamp.event(b);
-```
-
-Now stamp b is greater than all the others. We can do the same in stamp d to denote an update on ReplicaD:
-
-```java
-	d = d.event();
-	//or
-	d = Stamp.event(d);
-```
-
-These two stamps are now concurrent. Thus b.leq(d) is false and d.leq(b) is also false.
-
-Now suppose that we want to merge the updates in ReplicaB and ReplicaD. One way is to
-create a replica that reflects both updates:
-
-```java
-	b.join(d);
-	//or
-	b = Stamp.join(b,d);
-```
-
-This stamp e will now have an id that joins the ids in b and d, and has an event component that holds both issued events.  An alternative way, that keeps the number of replicas/stamps and does not form new ids, is to exchange events between both replicas.
-
-```java
-	b.join(d.peek());
-	d.join(Stamp.peek(b));
-```
-
-Now, stamps b and d are no longer concurrent and will compare as equivalent, since they depict the same events. 
+TODO: Add version vector simulation example in Erlang
--- src/itc.erl
+++ src/itc.erl
@@ -11,42 +11,48 @@
 %% 
 -module(itc).
 -author("Paulo Sergio Almeida <psa@di.uminho.pt>").
+
 -export([seed/0, event/1, join/2, fork/1, peek/1, leq/2]).
 -export([len/1, str/1, encode/1, decode/1]).
+
+-compile({no_auto_import,[min/2, max/2]}).
 -compile({inline, [{min,2}, {max,2}, {drop,2}, {lift,2}, {base,1},
-     {height,1}]}).
+                   {height,1}]}).
 
 seed() -> {1, 0}.
 
 join({I1, E1}, {I2, E2}) -> {sum(I1,I2), join_ev(E1, E2)}.
 
 fork({I, E}) ->
-{I1, I2} = split(I),
-{{I1, E}, {I2, E}}.
+    {I1, I2} = split(I),
+    {{I1, E}, {I2, E}}.
 
-peek({I, E}) -> {{0, E}, {I, E}}.
+peek({I, E}) ->
+    {{0, E}, {I, E}}.
 
 event({I, E}) ->
-{I,
-case fill(I, E) of
- E -> {_, E1} = grow(I, E), E1;
- E1 -> E1
-end
-}.
+    {I,
+     case fill(I, E) of
+        E ->
+            {_, E1} = grow(I, E),
+            E1;
+        E1 ->
+            E1
+     end}.
 
 leq({_, E1}, {_, E2}) -> leq_ev(E1, E2).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 leq_ev({N1, L1, R1}, {N2, L2, R2}) ->
-N1 =< N2 andalso
-leq_ev(lift(N1, L1), lift(N2, L2)) andalso
-leq_ev(lift(N1, R1), lift(N2, R2));
+    N1 =< N2 andalso
+    leq_ev(lift(N1, L1), lift(N2, L2)) andalso
+    leq_ev(lift(N1, R1), lift(N2, R2));
 
 leq_ev({N1, L1, R1}, N2) ->
-N1 =< N2 andalso
-leq_ev(lift(N1, L1), N2) andalso
-leq_ev(lift(N1, R1), N2);
+    N1 =< N2 andalso
+    leq_ev(lift(N1, L1), N2) andalso
+    leq_ev(lift(N1, R1), N2);
 
 leq_ev(N1, {N2, _, _}) -> N1 =< N2;
 
@@ -59,10 +65,11 @@ norm_id({0, 0}) -> 0;
 norm_id({1, 1}) -> 1;
 norm_id(X) -> X.
 
-norm_ev({N, M, M}) when is_integer(M) -> N + M;
+norm_ev({N, M, M}) when is_integer(M) ->
+    N + M;
 norm_ev({N, L, R}) ->
-M = min(base(L), base(R)),
-{N + M, drop(M, L), drop(M, R)}.
+    M = min(base(L), base(R)),
+    {N + M, drop(M, L), drop(M, R)}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -80,8 +87,8 @@ split({I1, I2}) -> {{I1, 0}, {0, I2}}.
 
 join_ev(E1={N1, _, _}, E2={N2, _, _}) when N1 > N2 -> join_ev(E2, E1);
 join_ev({N1, L1, R1}, {N2, L2, R2}) when N1 =< N2 ->
-D = N2 - N1,
-norm_ev({N1, join_ev(L1, lift(D, L2)), join_ev(R1, lift(D, R2))});
+    D = N2 - N1,
+    norm_ev({N1, join_ev(L1, lift(D, L2)), join_ev(R1, lift(D, R2))});
 join_ev(N1, {N2, L2, R2}) -> join_ev({N1, 0, 0}, {N2, L2, R2});
 join_ev({N1, L1, R1}, N2) -> join_ev({N1, L1, R1}, {N2, 0, 0});
 join_ev(N1, N2) -> max(N1, N2).
@@ -90,34 +97,33 @@ fill(0, E) -> E;
 fill(1, E={_, _, _}) -> height(E);
 fill(_, N) when is_integer(N) -> N;
 fill({1, R}, {N, El, Er}) ->
-Er1 = fill(R, Er),
-D = max(height(El), base(Er1)),
-norm_ev({N, D, Er1});
+    Er1 = fill(R, Er),
+    D = max(height(El), base(Er1)),
+    norm_ev({N, D, Er1});
 fill({L, 1}, {N, El, Er}) ->
-El1 = fill(L, El),
-D = max(height(Er), base(El1)),
-norm_ev({N, El1, D});
+    El1 = fill(L, El),
+    D = max(height(Er), base(El1)),
+    norm_ev({N, El1, D});
 fill({L, R}, {N, El, Er}) ->
-norm_ev({N, fill(L, El), fill(R, Er)}).
+    norm_ev({N, fill(L, El), fill(R, Er)}).
 
 grow(1, N) when is_integer(N)->
-{0, N + 1};
+    {0, N + 1};
 grow({0, I}, {N, L, R}) ->
-{H, E1} = grow(I, R),
-{H + 1, {N, L, E1}};
+    {H, E1} = grow(I, R),
+    {H + 1, {N, L, E1}};
 grow({I, 0}, {N, L, R}) ->
-{H, E1} = grow(I, L),
-{H + 1, {N, E1, R}};
+    {H, E1} = grow(I, L),
+    {H + 1, {N, E1, R}};
 grow({Il, Ir}, {N, L, R}) ->
-{Hl, El} = grow(Il, L),
-{Hr, Er} = grow(Ir, R),
-if
-Hl < Hr -> {Hl + 1, {N, El, R}};
-true ->    {Hr + 1, {N, L, Er}}
-end;
+    {Hl, El} = grow(Il, L),
+    {Hr, Er} = grow(Ir, R),
+    if Hl < Hr -> {Hl + 1, {N, El, R}};
+       true ->    {Hr + 1, {N, L, Er}}
+    end;
 grow(I, N) when is_integer(N)->
-{H, E} = grow(I, {N, 0, 0}),
-{H + 1000, E}.
+    {H, E} = grow(I, {N, 0, 0}),
+    {H + 1000, E}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -163,33 +169,56 @@ deci(<<3:2, B/bits>>) -> {L, B1} = deci(B), {R, B2} = deci(B1), {{L, R}, B2}.
 %ence({N, L, R}) -> <<0:1, 3:2, (ence(N))/bits, (ence(L))/bits, (ence(R))/bits>>;
 %ence(N) -> encn(N, 2, <<1:1>>).
 
-ence({0, 0, R}) -> <<0:1, 0:2, (ence(R))/bits>>;
-ence({0, L, 0}) -> <<0:1, 1:2, (ence(L))/bits>>;
-ence({0, L, R}) -> <<0:1, 2:2, (ence(L))/bits, (ence(R))/bits>>;
-ence({N, 0, R}) -> <<0:1, 3:2, 0:1, 0:1,
-                 (ence(N))/bits, (ence(R))/bits>>;
-ence({N, L, 0}) -> <<0:1, 3:2, 0:1, 1:1,
-                 (ence(N))/bits, (ence(L))/bits>>;
-ence({N, L, R}) -> <<0:1, 3:2, 1:1,
-                 (ence(N))/bits, (ence(L))/bits, (ence(R))/bits>>;
-ence(N) -> encn(N, 2).
+ence({0, 0, R}) ->
+    <<0:1, 0:2, (ence(R))/bits>>;
+ence({0, L, 0}) ->
+    <<0:1, 1:2, (ence(L))/bits>>;
+ence({0, L, R}) ->
+    <<0:1, 2:2, (ence(L))/bits, (ence(R))/bits>>;
+ence({N, 0, R}) ->
+    <<0:1, 3:2, 0:1, 0:1, (ence(N))/bits, (ence(R))/bits>>;
+ence({N, L, 0}) ->
+    <<0:1, 3:2, 0:1, 1:1, (ence(N))/bits, (ence(L))/bits>>;
+ence({N, L, R}) ->
+    <<0:1, 3:2, 1:1, (ence(N))/bits, (ence(L))/bits, (ence(R))/bits>>;
+ence(N) ->
+    encn(N, 2).
 
-dece(<<0:1, 0:2, B/bits>>) -> {R, B1} = dece(B), {{0,0,R}, B1};
-dece(<<0:1, 1:2, B/bits>>) -> {L, B1} = dece(B), {{0,L,0}, B1};
-dece(<<0:1, 2:2, B/bits>>) -> {L, B1} = dece(B), {R, B2} = dece(B1), {{0,L,R}, B2};
+dece(<<0:1, 0:2, B/bits>>) ->
+    {R, B1} = dece(B),
+    {{0,0,R}, B1};
+dece(<<0:1, 1:2, B/bits>>) ->
+    {L, B1} = dece(B),
+    {{0,L,0}, B1};
+dece(<<0:1, 2:2, B/bits>>) ->
+    {L, B1} = dece(B),
+    {R, B2} = dece(B1),
+    {{0,L,R}, B2};
 dece(<<0:1, 3:2, 0:1, 0:1, B/bits>>) ->
-{N, B1} = dece(B), {R, B2} = dece(B1), {{N, 0, R}, B2};
+    {N, B1} = dece(B),
+    {R, B2} = dece(B1),
+    {{N, 0, R}, B2};
 dece(<<0:1, 3:2, 0:1, 1:1, B/bits>>) ->
-{N, B1} = dece(B), {L, B2} = dece(B1), {{N, L, 0}, B2};
+    {N, B1} = dece(B),
+    {L, B2} = dece(B1),
+    {{N, L, 0}, B2};
 dece(<<0:1, 3:2, 1:1, B/bits>>) ->
-{N, B1} = dece(B), {L, B2} = dece(B1), {R, B3} = dece(B2), {{N, L, R}, B3};
-dece(<<1:1, B/bits>>) -> decn(B, 2, 0).
+    {N, B1} = dece(B),
+    {L, B2} = dece(B1),
+    {R, B3} = dece(B2),
+    {{N, L, R}, B3};
+dece(<<1:1, B/bits>>) ->
+    decn(B, 2, 0).
 
-encn(N, B) when N < (1 bsl B) -> <<((1 bsl B) - 2):B, N:B>>;
-encn(N, B) -> encn(N - (1 bsl B), B + 1).
+encn(N, B) when N < (1 bsl B) ->
+    <<((1 bsl B) - 2):B, N:B>>;
+encn(N, B) ->
+    encn(N - (1 bsl B), B + 1).
 
-decn(<<0:1, R/bits>>, B, Acc) -> <<N:B, R1/bits>> = R, {N+Acc, R1};
-decn(<<1:1, R/bits>>, B, Acc) -> decn(R, B+1, Acc + (1 bsl B)).
+decn(<<0:1, R/bits>>, B, Acc) ->
+    <<N:B, R1/bits>> = R, {N+Acc, R1};
+decn(<<1:1, R/bits>>, B, Acc) ->
+    decn(R, B+1, Acc + (1 bsl B)).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -209,7 +238,4 @@ stre({N, 0, R}) -> [stre(N), "R", stre(R)];
 stre({N, L, R}) -> [stre(N), "(L", stre(L), "+R", stre(R), ")"];
 stre(N) when N > 0 -> integer_to_list(N);
 stre(_) -> "".
-
-
-
 
